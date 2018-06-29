c964e28649a97d056340d9d16c5569e87de810da
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-06-28 22:11:53 -0700

    Fix tests

--- test/poison_test.exs
+++ test/poison_test.exs
@@ -2,8 +2,8 @@ defmodule Wand.PoisonTest do
   use ExUnit.Case, async: true
 
   test "encode/decode round trip" do
-    data = %{hello: "world"}
-    assert data = data
+    data = %{"hello" => "world"}
+    assert data == data
     |> Wand.Poison.encode!()
     |> Wand.Poison.decode!()
   end

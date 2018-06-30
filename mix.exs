ffef1404505e392a1eaf73df73d07bbed353fa0d
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-06-30 16:22:11 -0700

    Version 0.3.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule WandCore.MixProject do
   use Mix.Project
 
-  @version "0.2.2"
+  @version "0.3.0"
   @description "Global tasks for interacting with wand"
   @cli_env [
     coveralls: :test,
